070f0258c9d5b5f01af5fa5389d558bddb017d1f
Prajwal BG <prajwalbg05@gmail.com>
2026-02-24 10:28:29 +0530

    updated temperatureconvert code

--- Q7_temperatureconvert.py
+++ Q7_temperatureconvert.py
@@ -1,35 +1,59 @@
-def celcius_to_fahrenheit(celsius):
+# Temperature Converter
+
+def celsius_to_fahrenheit(celsius):
     return (celsius * 9/5) + 32
-def fahrenheit_to_celcius(fahrenheit):
+
+def fahrenheit_to_celsius(fahrenheit):
     return (fahrenheit - 32) * 5/9
-def celcius_to_kelvin(celsius):
+
+def celsius_to_kelvin(celsius):
     return celsius + 273.15
-def kelvin_to_celcius(kelvin):
+
+def kelvin_to_celsius(kelvin):
     return kelvin - 273.15
+
 def fahrenheit_to_kelvin(fahrenheit):
     return (fahrenheit - 32) * 5/9 + 273.15
+
 def kelvin_to_fahrenheit(kelvin):
     return (kelvin - 273.15) * 9/5 + 32
 
-options={
-    1:celcius_to_fahrenheit,
-    2:fahrenheit_to_celcius,
-    3:celcius_to_kelvin,
-    4:kelvin_to_celcius,
-    5:fahrenheit_to_kelvin,
-    6:kelvin_to_fahrenheit
-}
-print("Temperature Converter")
-print("1. Celcius to Fahrenheit")
-print("2. Fahrenheit to Celcius")
-print("3. Celcius to Kelvin")
-print("4. Kelvin to Celcius")
-print("5. Fahrenheit to Kelvin")
-print("6. Kelvin to Fahrenheit")
-choice=int(input("Enter your choice: "))        
-if choice in options:
-    temp=float(input("Enter the temperature: "))
-    result=options[choice](temp)
-    print("Converted temperature:", round(result, 2))
-else:
-    print("Invalid choice")
+
+while True:
+
+    print("\nTemperature Converter")
+    print("1. Celsius to Fahrenheit")
+    print("2. Fahrenheit to Celsius")
+    print("3. Celsius to Kelvin")
+    print("4. Kelvin to Celsius")
+    print("5. Fahrenheit to Kelvin")
+    print("6. Kelvin to Fahrenheit")
+    print("7. Exit")
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 7:
+        print("Exiting program.")
+        break
+
+    if choice >= 1 and choice <= 6:
+
+        temp = float(input("Enter the temperature: "))
+
+        if choice == 1:
+            result = celsius_to_fahrenheit(temp)
+        elif choice == 2:
+            result = fahrenheit_to_celsius(temp)
+        elif choice == 3:
+            result = celsius_to_kelvin(temp)
+        elif choice == 4:
+            result = kelvin_to_celsius(temp)
+        elif choice == 5:
+            result = fahrenheit_to_kelvin(temp)
+        elif choice == 6:
+            result = kelvin_to_fahrenheit(temp)
+
+        print("Converted temperature:", round(result, 2))
+
+    else:
+        print("Invalid choice. Please select between 1 and 7.")
